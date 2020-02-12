8a67d1347a68206ffc34ee44d5b859fd7477a7de
Geoff Jones <jones.l.geoff@gmail.com>
2020-02-11 16:36:22 -0800

    This didn't work

--- merge.py
+++ merge.py
@@ -4,3 +4,7 @@ f = open("Chris.txt", "r")
 print(f.read())
 f = open("Geoff", "r")
 print(f.read())
+def Merge("Chris.txt", "Geoff"):
+final_list = list1 + list2
+final_list.sort()
+return(final_list)
