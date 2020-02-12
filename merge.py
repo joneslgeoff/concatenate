fb429391819642659e1da8dbb84263ac8a9e2b8b
Geoff Jones <jones.l.geoff@gmail.com>
2020-02-11 19:27:51 -0800

    Removed dead code

--- merge.py
+++ merge.py
@@ -3,10 +3,7 @@ p2 = '/home/geoffwood/Desktop/Merger/Geoff'
 fp1 = open(p1)
 fp2 = open(p2)
 l1 = fp1.read().strip().split("\n")
-l1
 l2 = fp2.read().strip().split("\n")
-l2
 l3 = l1 + l2
-l3
 merge_list = sorted(l3)
 print(merge_list)
