8316f335826642ddd67439cf8061531122c3b707
Geoff Jones <jones.l.geoff@gmail.com>
2020-02-18 19:24:05 -0800

    First test case works

--- merge.py
+++ merge.py
@@ -1,9 +1,36 @@
-p1 = '/home/geoffwood/Desktop/Merger/Chris.txt'
-p2 = '/home/geoffwood/Desktop/Merger/Geoff'
-fp1 = open(p1)
-fp2 = open(p2)
-l1 = fp1.read().strip().split("\n")
-l2 = fp2.read().strip().split("\n")
-l3 = l1 + l2
-merge_list = sorted(l3)
-print(merge_list)
+def sort(seq):
+  '''TODO'''
+  return sorted(seq)
+
+def mergeMovieLists():
+  p1 = '/home/geoffwood/Desktop/Merger/Chris.txt'
+  p2 = '/home/geoffwood/Desktop/Merger/Geoff'
+  fp1 = open(p1)
+  fp2 = open(p2)
+  l1 = fp1.read().strip().split("\n")
+  l2 = fp2.read().strip().split("\n")
+  l3 = l1 + l2
+  merge_list = sort(l3)
+  print(merge_list)
+  #alphacheck(l3)
+
+
+def checkSorted(seq):
+  # TODO: implement me
+  # if empty list, then return True
+  # else: return False
+  
+  if len(seq) == 0:
+    return True
+
+def expectSorted(seq, shouldBeSorted):
+  print('  expecting that', seq, 'is sorted?', shouldBeSorted)
+  assert checkSorted(seq) == shouldBeSorted
+
+#mergeMovieLists()
+
+print('\nChecking sort() function ...')
+expectSorted([], True)
+#expectSorted([ 'aa' ], True)
+#expectSorted([ 'aa', 'bb' ], True)
+#expectSorted([ 'bb', 'aa' ], False)
