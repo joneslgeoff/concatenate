608543da77eb0604940c375cd26bb9d840f51bab
Geoff Jones <jones.l.geoff@gmail.com>
2020-02-28 16:26:03 -0800

    Extended checkSorted for arbitrary list length

--- merge.py
+++ merge.py
@@ -16,11 +16,18 @@ def mergeMovieLists():
 
 
 def checkSorted(seq):
+  # Base case: length is 0 or 1
   if len(seq) <= 1:
     return True
-  else:
+
+  # If the first two elements of `seq` are out of order, then `seq`
+  # is *not* sorted.
+  if not(seq[0] <= seq[1]):
     return False
-    #knock out the other list boys
+
+  # Recursive case: `seq` is sorted if the remainder of the sequence
+  # is sorted.
+  return checkSorted(seq[1:])  
 
 def expectSorted(seq, shouldBeSorted):
   print('  expecting that', seq, 'is sorted?', shouldBeSorted)
@@ -31,5 +38,9 @@ def expectSorted(seq, shouldBeSorted):
 print('\nChecking sort() function ...')
 expectSorted([], True)
 expectSorted([ 'aa' ], True)
-#expectSorted([ 'aa', 'bb' ], True)
-#expectSorted([ 'bb', 'aa' ], False)
+expectSorted([ 'aa', 'bb' ], True)
+expectSorted([ 'bb', 'aa' ], False)
+expectSorted([ 'cc', 'aa', 'bb' ], False)
+expectSorted([ 'aa', 'bb', 'cc' ], True)
+expectSorted([ 'aa', 'bb', 'cc', 'dd' ], True)
+expectSorted([ 'aa', 'cc', 'dd', 'bb' ], False)
